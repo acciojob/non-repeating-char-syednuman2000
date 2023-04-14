50738aa89f4890d3e802a79e4bec61b62ccf04f2
Syed Numan <111847221+syednuman2000@users.noreply.github.com>
2023-04-14 10:32:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function nonrepeat(s) {
+	let arr = [];
+    for(let i=0;i<26;i++){
+        arr.push(0);
+    }
+    for(let i=0;i<s.length;i++){
+        arr[s.charCodeAt(i)-97]++;
+    }
+    let ans = undefined;
+    for(let i=0;i<s.length;i++){
+        if(arr[s.charCodeAt(i)-97]==1) ans = s.charAt(i);
+    }
+    return ans;
+}
+let s = prompt("Enter a string");
+alert(non repeat(s);)
